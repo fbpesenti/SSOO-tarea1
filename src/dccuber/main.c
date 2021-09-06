50d5f1a7e0ee0283d1e0e243826a53ed5850b5e9
fbpesenti <fbpesenti@uc.cl>
2021-09-05 23:44:56 -0300

    avance rep

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <signal.h>
 #include "../file_manager/manager.h"
 
 int id_proceso_rep;
--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#include<stdbool.h>
+#include <stdbool.h>
+#include <signal.h>
 #include <unistd.h>
+
+int turnos[] = {0,0,0,0};
+
+void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
+{
+}
+
 
 //Esta funcion crea el output para el repartidor
 void output_file(int tiempo_semaforo1, int tiempo_semaforo2,int tiempo_semaforo3, int turnos_bodega, char* repartidor_name){
@@ -11,16 +19,40 @@ void output_file(int tiempo_semaforo1, int tiempo_semaforo2,int tiempo_semaforo3
 
 int main(int argc, char const *argv[])
 {
+  int distancia[4];
   int velocidad = 1;
-  int turnos[] = {0,0,0,0};
-  char estado_semaforo[4];
+  //se asume que todos los semaforos empiezan en verde 
+  char estado_semaforo[] = {"verde", "verde", "verde"};
   int señal;
   char estado_recibido;
+  
+  connect_sigaction(SIGUSR1, handle_sigusr1);
+
+  // aqui se recibe la señal correspondiente a cada semaforo con su cambio de color
+  // Semaforo 1 
   if (señal == 1)
   {
-    turnos[0] =+1;
+    // se cambia el estado del semaforo 1 correspondiente al color
     estado_semaforo[0] = estado_recibido;
-  };
+  }
+  else if(señal == 2)
+  {
+    turnos[1] =+1;
+    estado_semaforo[1] = estado_recibido;
+  }
+  else if(señal == 3)
+  {
+    turnos[1] =+1;
+    estado_semaforo[1] = estado_recibido;
+  }
+
+
+  alarm(velocidad);
+  
+  while (true)
+  ;
+
+  
   
   
 }
